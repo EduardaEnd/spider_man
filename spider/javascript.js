820aab067fa6fafda8b4beaa2d15b9b209e3e2c3
Eduarda de Barros Correia <dudabc162@gmail.com>
2025-08-16 22:29:21 -0300

    finalização spider man

--- spider/javascript.js
+++ spider/javascript.js
@@ -1,1 +1,36 @@
-function 
+const button1 = document.querySelector('#button1 > button');
+const letra_botao = document.querySelector('#button1 >button > a')
+const fundo = document.getElementById('imagem_fundo');
+const palavra = document.getElementById('palavra');
+const man = document.getElementById('man')
+
+function trocarPagina(pagina) {
+     fundo.style.opacity = 0;
+
+    setTimeout(() =>{
+  if (pagina === "pagina1") {
+    fundo.src = "/assets/imagem1.jpg";
+    button1.style.borderColor = "#CE1123";
+    letra_botao.style.color = "#CE1123";
+    palavra.textContent = 'Ficar';
+    man.style.webkitTextStroke = "2px #000000ff";
+
+  } 
+  else if (pagina === "pagina2") {
+    fundo.src = "/assets/pagina2.jpg";
+    button1.style.borderColor = "#000000ff";
+    letra_botao.style.color = "#000000ff";
+    palavra.textContent = 'Lutar?';
+    man.style.webkitTextStroke = "2px #000000ff";
+  } 
+  else if (pagina === "pagina3") {
+    fundo.src = "/assets/pagina3.jpg";
+    button1.style.borderColor = "#CE1123";
+    letra_botao.style.color = "#CE1123";
+    palavra.textContent = 'Confiar';
+    man.style.webkitTextStroke = "1px #f0f8ff";
+  }
+
+    fundo.style.opacity = 1;
+}, 300);
+}
